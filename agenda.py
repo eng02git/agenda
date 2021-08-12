4d3d627e6262c2f98a854ec707c419746b2cc3d8
eng02git <87899916+eng02git@users.noreply.github.com>
2021-08-12 14:15:23 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -57,8 +57,8 @@ def main():
 	# The file token.json stores the user's access and refresh tokens, and is
 	# created automatically when the authorization flow completes for the first
 	# time.
-	if os.path.exists('token.json'):
-		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	#if os.path.exists('token.json'):
+	creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 		
 	# If there are no (valid) credentials available, let the user log in.
 	if not creds or not creds.valid:
