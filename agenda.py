0deef1a37f9858e912e94ea3d98ab3ab9c56796a
eng02git <87899916+eng02git@users.noreply.github.com>
2021-08-12 13:15:34 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -19,6 +19,7 @@ st.set_page_config(
 )
 
 key_dict = json.loads(st.secrets["textkey"])
+credentials_json = json.loads(st.secrets["text_key"])
 
 config = {
     "apiKey": "AIzaSyBn2VArLQx0z6ChrD5fB52Zo_eTDBysBF8",
@@ -58,7 +59,7 @@ def main():
 			creds.refresh(Request())
 		else:
 			flow = InstalledAppFlow.from_client_secrets_file(
-				'credentials.json', SCOPES)
+				credentials_json, SCOPES)
 			creds = flow.run_local_server(port=0)
 		# Save the credentials for the next run
 		#with open('token.json', 'w') as token:
