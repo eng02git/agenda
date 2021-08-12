de7c1b53298b5070fae14b531c9cabb0260fffc1
eng02git <87899916+eng02git@users.noreply.github.com>
2021-08-12 13:19:34 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -59,7 +59,7 @@ def main():
 			creds.refresh(Request())
 		else:
 			flow = InstalledAppFlow.from_client_secrets_file(
-				credentials_json, SCOPES)
+				credentials_json.decode("utf-8"), SCOPES)
 			creds = flow.run_local_server(port=0)
 		# Save the credentials for the next run
 		#with open('token.json', 'w') as token:
