b32fd414672b79fcb689d214214ca58700e97b34
eng02git <87899916+eng02git@users.noreply.github.com>
2021-08-16 12:00:37 -0300

    Update agenda_pc.py

--- agenda_pc.py
+++ agenda_pc.py
@@ -100,7 +100,7 @@ def main():
 		ev0_, ev1_, ev2_, ev3_, ev4_, ev5_, ev6_, ev7_ = st.columns(8)
 		
 		# colunas eventos da fixos e da semana
-		fixo, semana, data_semana = st.columns([3, 2, 3])
+		fixo, semana, data_semana = st.columns([3, 5])
 		d1, d2, d3, s1, s2, s3 = st.columns([1,1.5,0.5,1,2.5,1.5])
 		#d1_, d2_, d3_, d4_, s1_, s2_, s3_, s4_ = st.columns(8)
 		
