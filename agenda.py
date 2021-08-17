991c72331cafee6524c390d20f8e22a1e5349044
eng02git <87899916+eng02git@users.noreply.github.com>
2021-08-17 14:12:41 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -26,9 +26,7 @@ def main():
 	"""Shows basic usage of the Google Calendar API.
 	st.writes the start and name of the next 10 events on the user's calendar.
 	"""
-	creds = None
-	if os.path.exists('tokens.json'):
-		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
 	# If there are no (valid) credentials available, let the user log in.
 	if not creds or not creds.valid:
