79c500528b6d2796bae3103f127e4585ce35d29e
eng02git <87899916+eng02git@users.noreply.github.com>
2021-08-24 16:44:06 -0300

    Update agenda_pc.py

--- agenda_pc.py
+++ agenda_pc.py
@@ -36,16 +36,16 @@ def main():
 	# carrega css para estilo
 	local_css("style.css")
 
-	creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	#creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
 	# If there are no (valid) credentials available, let the user log in.
-	if not creds or not creds.valid:
-		if creds and creds.expired and creds.refresh_token:
-			creds.refresh(Request())
-		else:
-			flow = InstalledAppFlow.from_client_secrets_file(
-				'credentials.json', SCOPES)
-			creds = flow.run_local_server(port=0)
+	#if not creds or not creds.valid:
+	#	if creds and creds.expired and creds.refresh_token:
+	#		creds.refresh(Request())
+	#	else:
+	#		flow = InstalledAppFlow.from_client_secrets_file(
+	#			'credentials.json', SCOPES)
+	#		creds = flow.run_local_server(port=0)
 
 	service = build('calendar', 'v3', credentials=credentials)
 
